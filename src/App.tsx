4ef724dc24e55abac618791970dd733234b60801
Shivam67Code <evilsniper64@gmail.com>
2025-09-21 10:44:21 +0545

    Added wordsList

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+function App() {
+  const[wordToGuess,setWordToGuess] = useState("Test")
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
